506eb5623baebc5a35826b4403c556b32dfcb1f7
andrearosasco <andrearosasco.ar@gmail.com>
2023-03-24 10:05:43 +0100

    moved pc extraction to segmentation this was done so that the segmentation node could control the gaze controller Also, the segmentation output can be NOT_OBSERVED when there are not enough points or the box is too distant

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -1,10 +1,14 @@
 import copy
 import sys
-import time
 from pathlib import Path
 
 import cv2
+import numpy as np
 from loguru import logger
+from scipy.spatial.transform import Rotation
+
+from utils.concurrency.utils.signals import Signals
+from grasping.utils.input import RealSense
 
 sys.path.insert(0, Path(__file__).parent.parent.as_posix())
 
@@ -24,6 +28,8 @@ class Segmentation(Network.node):
     def __init__(self):
         super().__init__(**Network.Args.to_dict())
         self.seg_model = None
+        self.follow_object = True
+        self.R = Rotation.from_euler('xyz', [-90, 0, 0], degrees=True).as_matrix()
         self.timer = Timer(window=10)
 
     def startup(self):
@@ -37,18 +43,51 @@ class Segmentation(Network.node):
         logger.info("Read camera input", recurring=True)
 
         rgb = data['rgb']
+        if rgb in Signals:
+            return output
 
         # Segment the rgb and extract the object depth
         # start = time.perf_counter()
         mask = self.seg_model(rgb)
+
         # print(1 / (time.perf_counter() - start))
         mask = cv2.resize(mask, dsize=(640, 480), interpolation=cv2.INTER_NEAREST)
 
         logger.info("RGB segmented", recurring=True)
 
-        output['mask'] = mask
+        segmented_depth = copy.deepcopy(data['depth'])
+        segmented_depth[mask != 1] = 0
 
-        return output
+        logger.info("Depth segmented", recurring=True)
+
+        # There are not enough points
+        if len(segmented_depth.nonzero()[0]) < 4096:
+            output['mask'] = Signals.NOT_OBSERVED
+            self.write('to_visualizer', output)
+            self.write('to_shape_completion', {'segmented_pc': Signals.NOT_OBSERVED})
+            logger.warning('Warning: not enough input points. Skipping reconstruction', recurring=True)
+            return
+
+        distance = segmented_depth[segmented_depth != 0].min()
+        output['obj_distance'] = int(distance)
+
+        # The box is too distant
+        if distance > 700:
+            output['mask'] = Signals.NOT_OBSERVED
+            self.write('to_visualizer', output)
+            self.write('to_shape_completion', {'segmented_pc': Signals.NOT_OBSERVED})
+            return
+
+        output['mask'] = mask
+        segmented_pc = RealSense.depth_pointcloud(segmented_depth)
+
+        self.write('to_visualizer', output)
+        self.write('to_shape_completion', {'segmented_pc': segmented_pc})
+
+        point = np.mean(segmented_pc, axis=0, keepdims=True)
+        self.write('to_gaze_control', {'point': point})
+        if self.follow_object:
+            self.write('to_3d_viz', {'center': point})
 
 
 if __name__ == '__main__':
