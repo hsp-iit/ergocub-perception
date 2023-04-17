6e67b91ad790bb6f8b59fbb6467d67aac565e925
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-04-17 17:15:54 +0200

    added pose tracking on the partial pc and skipping reconstruction if just 1 plane

--- scripts/grasping_pipeline.py
+++ scripts/grasping_pipeline.py
@@ -26,24 +26,6 @@ from grasping.grasp_detection.ransac_gd.trt.trt_ransac import TrTRansac, RansacT
 setup_logger(**Logging.Logger.Params.to_dict())
 
 
-# class Watch():
-#     def __init__(self):
-#         self.ft = defaultdict(lambda: 0)
-#     def check(self):
-#         from configs import grasping_config
-#
-#         # while True:
-#         for file in Path('configs').glob('*'):
-#             mt = os.path.getmtime(file.as_posix())
-#
-#             if mt != self.ft[file.name] and self.ft[file.name] != 0:
-#                 reload_package(grasping_config)
-#                 from configs import grasping_config
-#                 global Config
-#                 Config = grasping_config.Config.Grasping
-#                 logger.success('Configuration reloaded')
-#             self.ft[file.name] = mt
-
 class Grasping(Network.node):
     def __init__(self):
         super().__init__(**Network.Args.to_dict())
@@ -56,8 +38,7 @@ class Grasping(Network.node):
 
         self.max_partial_points = 0
 
-        self.prev_denormalize = None
-        self.action = {"action": "none"}
+        self.prev_output = None
 
         self.timer = Timer(window=10)
 
@@ -68,10 +49,11 @@ class Grasping(Network.node):
         self.pcr_encoder = ShapeCompletion.Encoder.model(**ShapeCompletion.Encoder.Args.to_dict())
         self.pcr_decoder = ShapeCompletion.Decoder.model(**ShapeCompletion.Decoder.Args.to_dict())
         self.ransac = TrTRansac(RANSAC.Args.engine_path)
+        self.partial_rs_tracker = RansacTracker(**RANSAC.Tracker.to_dict(), debug=True)
         self.grasp_detector = GraspDetection.model()
 
     def loop(self, data):
-        output = {}
+        output = data
 
         self.timer.start()
         # Input
@@ -115,10 +97,42 @@ class Grasping(Network.node):
             pad = np.zeros([diff, 3])
             pad[:] = segmented_pc[0]
             size_pc = np.vstack((denoised_pc, pad))
+            
+        # Count planes
+        partial_box = self.ransac(size_pc, RANSAC.Args.tolerance, RANSAC.Args.iterations, num_planes=2)
+        if partial_box is None:
+            logger.warning('Ransac couldn\'t detect more than 1 plane in the input point cloud', recurring=True)
+            output['fps_od'] = 1 / self.timer.compute(stop=True)
+            self.prev_output = copy.deepcopy(output)
+            return output
+        
+        _, aux = self.partial_rs_tracker(partial_box, size_pc)
+        # If they are different it means we are using the old one (the inputs are quite similar)
+        #   so we don't recompute everything and just return the old outputs
+        if not np.all(aux == size_pc):
+            logger.info('Using previous iteration output', recurring=True)
+            output = self.prev_output
+            output['rgb'] = data['rgb']
+            output['depth'] = data['depth']
+            output['fps_od'] = 1 / self.timer.compute(stop=True)
+            return output
 
         # Normalize
         mean = np.mean(size_pc, axis=0)
         var = np.sqrt(np.max(np.sum((size_pc - mean) ** 2, axis=1)))
+        
+        
+        # if self.old_mean is not None:
+        #     print(np.linalg.norm(mean - self.old_mean))
+        #     print(np.linalg.norm(var - self.old_var))
+        #     if np.linalg.norm(mean - self.old_mean) < 0.05:
+        #         mean = self.old_mean
+        #     if np.linalg.norm(var - self.old_var) < 0.05:
+        #         var = self.old_var
+            
+        # self.old_mean = mean
+        # self.old_var = var
+        
         normalized_pc = (size_pc - mean) / (var * 2)
         normalized_pc[..., -1] = -normalized_pc[..., -1]
 
@@ -136,6 +150,8 @@ class Grasping(Network.node):
 
             output['fps_od'] = 1 / self.timer.compute(stop=True)
             return output
+        
+        output['reconstruction'] = reconstruction
 
         center = np.mean(
             (np.block(
@@ -159,6 +175,7 @@ class Grasping(Network.node):
         if poses is None:
             logger.warning('Corrupted reconstruction - check the input point cloud', recurring=True)
             output['fps_od'] = 1 / self.timer.compute(stop=True)
+            self.prev_output = copy.deepcopy(output)
             return output
 
         hands_camera_frame = np.stack([compose_transformations([poses[1].T, poses[0][np.newaxis] * (var * 2) + mean, R]),
@@ -169,8 +186,6 @@ class Grasping(Network.node):
         if 'camera_pose' in data:
             hands_root_frame = np.stack([camera_pose @ compose_transformations([poses[1].T, poses[0][np.newaxis] * (var * 2) + mean, R]),
                                          camera_pose @ compose_transformations([poses[3].T, poses[2][np.newaxis] * (var * 2) + mean, R])], axis=-1)
-
-        if 'camera_pose' in data:
             output['hands_root_frame'] = hands_root_frame
         # hands_normalized = np.stack([compose_transformations([poses[1].T, poses[0][np.newaxis]]),
         #                              compose_transformations([poses[3].T, poses[2][np.newaxis]])], axis=-1)
@@ -180,10 +195,10 @@ class Grasping(Network.node):
             output['lines'] = poses[5]
             output['vertices'] = poses[6] * (var * 2) + mean  # de-normalized
             output['partial'] = normalized_pc
-            output['reconstruction'] = reconstruction
             output['transform'] = denormalize
 
         output['fps_od'] = 1 / self.timer.compute(stop=True)
+        self.prev_output = copy.deepcopy(output)
         return output
 
 
