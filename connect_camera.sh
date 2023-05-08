41d7276e201221a3d3af7b2357cd6caa24f6ce76
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-05-08 09:29:50 +0200

    restored rgb, depth compression

--- connect_camera.sh
+++ connect_camera.sh
@@ -1,4 +1,5 @@
-yarp connect /depthCamera/rgbImage:o /depthCamera/rgbImage:r  # mjpeg
-yarp connect /depthCamera/depthImage:o /depthCamera/depthImage:r  #  tcp+send.portmonitor+file.depthimage_compression_zlib+recv.portmonitor+file.depthimage_compression_zlib+type.dll
+yarp connect /depthCamera/rgbImage:o /depthCamera/rgbImage:r mjpeg
+yarp connect /depthCamera/depthImage:o /depthCamera/depthImage:r tcp+send.portmonitor+file.depthimage_compression_zlib+recv.portmonitor+file.depthimage_compression_zlib+type.dll
+
 
 
